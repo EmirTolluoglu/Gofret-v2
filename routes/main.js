66f7d434079245254bb0a2a874e9588a1345233d
Emir <71454370+EmirTolluoglu@users.noreply.github.com>
2022-11-21 01:01:24 +0300

    skeloten

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,20 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/explore');
+});
+
+router.get('/explore', (req, res) => {
+    res.render('explore', { title: 'Explore' });
+});
+
+router.get('/trade', (req, res) => {
+    res.render('trade', { layout: 'multi-step-form', title: 'Trade' });
+});
+
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: 'Profile' });
 });
 
 
